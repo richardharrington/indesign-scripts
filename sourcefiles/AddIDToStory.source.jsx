d0f181eecb61bb923dc86a9430449e756019e8e3
Richard Harrington <harrington.richard@gmail.com>
2011-12-07 13:57:24 -0500

    Changes to AddIDToStory and EditOnWeb, also small changes to main README
    • Changed name of AddIDToStory to AddURLToStory
    • Made AddURLToStory and EditOnWeb more general, so that they're not just for use on our particular CMS
    • Added some stuff to README file

--- sourcefiles/AddIDToStory.source.jsx
+++
@@ -1,26 +0,0 @@
-// This function adds an identifier to a story in InDesign or 
-// InCopy which allows the script EditOnWeb to find the appropriate
-// page on the CMS, for anyone with their cursor in the story.
-
-function error_exit (message) {  if (arguments.length > 0) alert (unescape(message));  exit();} 	
-	
-		var mySelection = app.selection[0];
-if (typeof mySelection == 'undefined') error_exit ('Please select something and try again.');switch (mySelection.constructor.name) {	case "Character":	case "Word":	case "TextStyleRange":	case "Line":	case "Paragraph":	case "TextColumn":	case "Text":	case "InsertionPoint" :	case "TextFrame" :
-	  var myStory = mySelection.parentStory;
-	  var myInput = prompt ("What is the ID number (or the whole URL) of the web page in the CMS that corresponds to this article?", "");
-	  if (myInput == null) exit();
-	  var myMatch = myInput.match(/\D/);  // null if it's a number
-	  if (myMatch == null) {
-	    myStory.label = "http://www.forward.com/symphony/publish/articles/edit/" + myInput;
-	    exit();
-	  }
-	  myMatch = myInput.match(/http:\/\/www\.forward\.com/);
-	  if (myMatch != null) {
-	    myStory.label = myInput;
-	    exit();
-	  }
-	  
-	  // default
-	  error_exit ('Please try again and type in a Forward URL or an article ID number (numbers only, no spaces or punctuation.)');
-}
-	  
---
+++ sourcefiles/AddURLToStory.source.jsx
@@ -0,0 +1,64 @@
+// This script associates a URL with a story in InDesign or 
+// InCopy via the insertLabel method, which then allows the script 
+// EditOnWeb to open a web browser to that URL.
+
+// This is useful for editing stories in a CMS.
+// You can cut and paste the URL of the story's CMS entry into 
+// the prompt dialog box in this script, and then the story is tied 
+// to that page forever.
+
+function equalsIn( value, array ) {
+    for (var i = 0, len = array.length; i < len; i++) {
+        if (value === array[i]) return true;
+    }
+    return false;
+}
+
+function alertExit( message ) {
+    if (message) {
+        alert( unescape( message ));
+    }
+    exit();
+}
+
+if (app.selection.length === 0) {
+	alertExit( 'Please select something and try again.' );
+} else if (app.selection.length > 1) {
+    alertExit( 'Please select some text or a text frame (only one) and try again.');
+}
+
+
+var mySelection = app.selection[0];
+if (equalsIn( mySelection.constructor.name, 
+             ["Character", "Word", "TextStyleRange", "Line",
+              "Paragraph", "TextColumn", "Text",
+              "InsertionPoint", "TextFrame", "Story"])) {
+	
+    var myStory = (mySelection.constructor.name === "Story") ? mySelection :
+                   mySelection.parentStory;
+    var myInput = prompt ("What is the URL that you want associated with this story?", 
+                           myStory.extractLabel( "URI" )); // if no existing label, 
+                                                           // it's automatically the empty string
+    if (myInput === null) {
+	    exit();
+	}
+
+	// Check if it's a reasonable URL.
+	if (myInput.match( /(https?\:\/\/)?[a-zA-Z0-9_\-\.]+\.[a-zA-Z]{2,4}(\/\S*)?$/ ) ) {
+    
+	    // If the person neglected the "http://", add it
+		if (!myInput.match( /^https?\:\/\// )) {
+		    myInput = "http://" + myInput;
+		}
+	
+        myStory.insertLabel( "URI", myInput );
+        exit();
+        
+    } else {
+        alertExit( "Please try again and type in a valid URL." );
+    }
+    
+} else {
+    alertExit( "Please select some text or a text frame and try again." );
+}
+	  
