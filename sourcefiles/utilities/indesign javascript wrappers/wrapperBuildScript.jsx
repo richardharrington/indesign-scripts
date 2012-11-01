6fdda9b365c1a43037881049bc208bcaaca49df0
Richard Harrington <harrington.richard@gmail.com>
2012-11-01 10:54:31 -0300

    corrected description in comments of wrapperBuildSript

--- sourcefiles/utilities/indesign javascript wrappers/wrapperBuildScript.jsx
+++ sourcefiles/utilities/indesign javascript wrappers/wrapperBuildScript.jsx
@@ -1,1 +1,35 @@
-﻿// Set to 7.0 scripting object model (Indesign CS5)app.scriptPreferences.version = 7.0;/* This script asks the user for a folder   and unlinks all the stories in all the InDesign   documents in that folder from their InCopy source files.      This is useful when you have completed a project   and published it, and you don't want any editors    messing with the InDesign documents in InCopy,   under the mistaken impression that their changes will   be included in the final version of the project.   */#include ../util.jsxvar util = FORWARD.Util;// ------------------------------------------------------------------------------------// Get the folders.var thisFolder = app.activeScript.parent; var sourceFilesFolder = thisFolder.parent.parent;var topLevelFolder = sourceFilesFolder.parent;// Set the wrapper build script (MAKE THIS A CONFIG AT THE TOP).var wrapperScriptTemplateFile = File( thisFolder.fullName + '/wrapperScriptTemplate.jsx' );    // Get the list of all sourcefiles in the sourcefiles folder.var sourceFiles = sourceFilesFolder.getFiles("*.source.jsx");util.forEach( sourceFiles, function( file ) {	wrapperFile = File( (topLevelFolder.fullName + '/' + file.name.replace( '.source', '' )));	if (!wrapperFile.exists) {		wrapperScriptTemplateFile.copy( wrapperFile );	}});	
+﻿// Set to 7.0 scripting object model (Indesign CS5)
+app.scriptPreferences.version = 7.0;
+
+
+/* This script creates identical (except for the filenames) top-level
+   wrapper scripts for all the scripts in the sourcefiles folder.
+   These wrapper scripts can then be run in place of the other scripts.
+   The benefit of this is that they appear as one step in the undo menu.
+   
+*/
+
+#include ../util.jsx
+
+var util = FORWARD.Util;
+
+
+// ------------------------------------------------------------------------------------
+
+// Get the folders.
+var thisFolder = app.activeScript.parent; 
+var sourceFilesFolder = thisFolder.parent.parent;
+var topLevelFolder = sourceFilesFolder.parent;
+
+// Set the wrapper build script (MAKE THIS A CONFIG AT THE TOP).
+var wrapperScriptTemplateFile = File( thisFolder.fullName + '/wrapperScriptTemplate.jsx' );
+    
+// Get the list of all sourcefiles in the sourcefiles folder.
+var sourceFiles = sourceFilesFolder.getFiles("*.source.jsx");
+
+util.forEach( sourceFiles, function( file ) {
+	wrapperFile = File( (topLevelFolder.fullName + '/' + file.name.replace( '.source', '' )));
+	if (!wrapperFile.exists) {
+		wrapperScriptTemplateFile.copy( wrapperFile );
+	}
+});	
